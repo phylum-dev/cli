4470737daef599c787775fe789150c91d56ac3d8
Eric Freitag <eric@phylum.io>
2021-05-12 20:21:00 -0500

    Update path to bash completions for install script

--- lib/install.sh
+++ lib/install.sh
@@ -16,16 +16,14 @@ if [ ! -f ${HOME}/.phylum/settings.yaml ]; then
   fi
 fi
 
-if [ ! -f ${HOME}/.phylum/phylum-cli.bash ]; then
-  if [ -f src/bin/phylum-cli.bash ]; then
-    echo '[*] Copying phylum-cli.bash to ~/.phylum'
-    cp -f src/bin/phylum-cli.bash ${HOME}/.phylum/
-  elif [ -f phylum-cli.bash ]; then
-    echo '[*] Copying phylum-cli.bash to ~/.phylum'
-    cp -f phylum-cli.bash ${HOME}/.phylum/
-  else
-    echo "Can't find phylum-cli.bash"
-  fi
+if [ -f src/bin/phylum.bash ]; then
+	echo '[*] Copying phylum.bash to ~/.phylum'
+	cp -f src/bin/phylum.bash ${HOME}/.phylum/
+elif [ -f phylum.bash ]; then
+	echo '[*] Copying phylum.bash to ~/.phylum'
+	cp -f phylum.bash ${HOME}/.phylum/
+else
+	echo "Can't find phylum.bash"
 fi
 
 if [ -f target/release/phylum ]; then
@@ -38,9 +36,9 @@ else
   echo "Can't find phylum"
 fi
 
-if ! grep -q 'phylum-cli.bash' $HOME/.bashrc ;
+if ! grep -q 'phylum.bash' $HOME/.bashrc ;
 then
-  echo "source \$HOME/.phylum/phylum-cli.bash" >> ${HOME}/.bashrc
+  echo "source \$HOME/.phylum/phylum.bash" >> ${HOME}/.bashrc
 fi
 if ! grep -q '.phylum/:\$PATH' $HOME/.bashrc;
 then
