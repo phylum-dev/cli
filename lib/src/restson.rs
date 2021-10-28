abeddc8f73951c65cddbbdfd08ca86f14f2476c7
Eric Freitag <eric@phylum.io>
2021-10-28 15:26:25 -0500

    Add a config file option for ignoring cert check

--- lib/src/restson.rs
+++ lib/src/restson.rs
@@ -281,7 +281,6 @@ impl RestClient {
             Some(client) => client,
             None => {
                 if builder.ignore_certs {
-                    log::warn!("Not validating server certificate per user request.");
                     Client::builder().build(HttpsConnector::without_cert_validation())
                 } else {
                     Client::builder().build(HttpsConnector::with_native_roots())
