40c1b8fa53a5af117828ca1f7495832225adda5a
Charles Coggins <charles@phylum.io>
2022-03-11 10:34:19 -0600

    Update release script to work with new paths and format for shellcheck

--- release.sh
+++ release.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
 printf "version: "
-read version
+read -r version
 
 printf "changelog: "
-read changelog
+read -r changelog
 
 sed -E -i "1 s#^#* $version - $changelog\n#" CHANGELOG
-sed -E -i "s/^version = \"([^\"]*)\"/version = \"$version\"/" lib/Cargo.toml
-sed -E -i "s/^version: \"([^\"]*)\"/version: \"$version\"/" lib/src/bin/.conf/cli.yaml
+sed -E -i "s/^version = \"([^\"]*)\"/version = \"$version\"/" cli/Cargo.toml
 git add CHANGELOG
-git add lib/Cargo.toml
-git add lib/src/bin/.conf/cli.yaml
+git add cli/Cargo.toml
 git commit -m "Bump version"
-
-#sed -E -i "0,/^$/s/^version = \"([^\"]*)\"/version = \"$version\"/" bindings/python/Cargo.toml
 
 TAG=v${version}
 
-echo Tagging / pushing ${TAG}, press any key to proceed...
-read
+echo Tagging / pushing "${TAG}", press any key to proceed...
+read -r
 git push
-git tag ${TAG}
+git tag "${TAG}"
 git push --tags
-
