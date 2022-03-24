ed67008f072eff4eadc147f11cfff90be92f36ad
Charles Coggins <charles@phylum.io>
2022-03-24 15:36:39 -0500

    Update install.sh script
    * Ensure all tabs are 4 spaces
    * Remove trailing whitespace
    * Adhere to shellcheck for QA
    * Remove dead code
    * Ensure RC files exist before modifying them
    * Ensure the patch_zshrc function works even when not in $HOME

--- cli/src/install.sh
+++ cli/src/install.sh
@@ -7,12 +7,12 @@ NC='\033[0m'
 # Don't continue after failure:
 set -euo pipefail
 
-error() { 
-    echo -e "${RED}ERROR${NC} ${1}" 
+error() {
+    echo -e "${RED}ERROR${NC} ${1}"
 }
 
-success() { 
-    echo -e "${GREEN}   OK${NC} ${1}" 
+success() {
+    echo -e "${GREEN}   OK${NC} ${1}"
 }
 
 banner() {
@@ -32,32 +32,21 @@ get_platform() {
     fi
 }
 
-# Get the platform name.
-get_arch() {
-    local platform_arch
-    platform_arch=$(uname -p)
-    if [[ "${platform_arch:0:3}" == "arm" ]]; then
-        echo "aarch64"
-    else
-        echo "x86_64"
-    fi
-}
-
 get_rc_file() {
-  case $(basename "$SHELL") in
-    bash)
-      echo "${HOME}/.bashrc"
-      ;;
-    zsh)
-      echo "${HOME}/.zshrc"
-      ;;
-    fish)
-      echo "${HOME}/.config/fish/config.fish"
-      ;;
-    *)
-      echo "your shell's configuration file"
-      ;;
-  esac
+    case $(basename "$SHELL") in
+        bash)
+            echo "${HOME}/.bashrc"
+            ;;
+        zsh)
+            echo "${HOME}/.zshrc"
+            ;;
+        fish)
+            echo "${HOME}/.config/fish/config.fish"
+            ;;
+        *)
+            echo "shell's configuration file"
+            ;;
+    esac
 }
 
 # Check if the provided path exists and copy to the specified location.
@@ -75,88 +64,90 @@ check_copy() {
 }
 
 add_to_path_and_alias() {
-  rc_path=${1}
-  shell=${2}
+    rc_path=${1}
+    shell=${2}
 
-  if ! grep -q '.phylum:\$PATH' "${rc_path}"; then
-    export PATH="$HOME/.phylum:$PATH"
-    echo 'export PATH="$HOME/.phylum:$PATH"' >> "${rc_path}"
-    success "Updated ${shell}'s \$PATH to include ${HOME}/.phylum/."
-  fi
+    # shellcheck disable=SC2016 # we don't want to expand $PATH here
+    if ! grep -q '.phylum:\$PATH' "${rc_path}"; then
+        export PATH="$HOME/.phylum:$PATH"
+        echo 'export PATH="$HOME/.phylum:$PATH"' >> "${rc_path}"
+        success "Updated ${shell}'s \$PATH to include ${HOME}/.phylum/."
+    fi
 
-  if ! grep -q 'alias ph=' "${rc_path}"; then
-      echo "alias ph='phylum'" >> "${rc_path}"
-      success "Created ph alias for phylum in ${shell}." 
-  fi
+    if ! grep -q 'alias ph=' "${rc_path}"; then
+        echo "alias ph='phylum'" >> "${rc_path}"
+        success "Created ph alias for phylum in ${shell}."
+    fi
 }
 
 patch_zshrc() {
-  if [ -f "${HOME}/.zshrc" ]; then
-      rc_path=".zshrc"
+    rc_path="${HOME}/.zshrc"
 
-      if ! grep -q '.phylum/completions' "$HOME/${rc_path}"; then
-          mkdir -p "$HOME/.phylum/completions"
-          echo "fpath+=(\"$HOME/.phylum/completions\")" >> "${HOME}/${rc_path}"
-      fi
-      if ! grep -q 'autoload -U compinit && compinit' "$HOME/${rc_path}"; then
-          echo "autoload -U compinit && compinit" >> "${HOME}/${rc_path}"
-      fi
+    if [ ! -f "${rc_path}" ]; then
+        touch "${rc_path}"
+    fi
 
-      success "Enabled completions for zsh."
+    if ! grep -q '.phylum/completions' "${rc_path}"; then
+        mkdir -p "$HOME/.phylum/completions"
+        echo "fpath+=(\"\$HOME/.phylum/completions\")" >> "${rc_path}"
+    fi
+    if ! grep -q 'autoload -U compinit && compinit' "${rc_path}"; then
+        echo "autoload -U compinit && compinit" >> "${rc_path}"
+    fi
+    success "Completions are enabled for zsh."
 
-      add_to_path_and_alias "${rc_path}" "zsh"
-  fi
+    add_to_path_and_alias "${rc_path}" "zsh"
 }
 
 patch_bashrc() {
-  if [ -f "${HOME}/.bashrc" ]; then
-      rc_path="${HOME}/.bashrc"
+    rc_path="${HOME}/.bashrc"
 
-      if ! grep -q '.phylum/completions/phylum.bash' "${rc_path}"; then
-          echo "source \$HOME/.phylum/completions/phylum.bash" >> "${rc_path}"
-      fi
+    if [ ! -f "${rc_path}" ]; then
+        touch "${rc_path}"
+    fi
 
-      success "Enabled completions for bash."
+    if ! grep -q '.phylum/completions/phylum.bash' "${rc_path}"; then
+        echo "source \$HOME/.phylum/completions/phylum.bash" >> "${rc_path}"
+    fi
+    success "Completions are enabled for bash."
 
-      add_to_path_and_alias "${rc_path}" "bash"
-  fi
+    add_to_path_and_alias "${rc_path}" "bash"
 }
 
 create_directory() {
-  # Create the config directory if one does not already exist.
-  if [ ! -d "${HOME}/.phylum" ]; then
-      mkdir -p "${HOME}/.phylum"
-      success "Created directory .phylum in home directory."
-  fi
+    # Create the config directory if one does not already exist.
+    if [ ! -d "${HOME}/.phylum" ]; then
+        mkdir -p "${HOME}/.phylum"
+        success "Created directory .phylum in home directory."
+    fi
 }
 
 copy_files() {
-  # Copy the specific platform binary.
-  platform=$(get_platform)
-  arch=$(get_arch)
-  workdir=$(dirname "$0")
-  bin_name="phylum"
+    # Copy the specific platform binary.
+    platform=$(get_platform)
+    bin_name="phylum"
 
-  if [[ "$platform" == "macos" ]]; then
-      cat "${bin_name}" > "${HOME}/.phylum/phylum"
-      success "Copied ${bin_name} to ${HOME}/.phylum/phylum."
-  else
-      check_copy "${bin_name}" "${HOME}/.phylum/phylum"
-  fi
-  chmod +x "${HOME}/.phylum/phylum"
+    if [[ "$platform" == "macos" ]]; then
+        cat "${bin_name}" > "${HOME}/.phylum/phylum"
+        success "Copied ${bin_name} to ${HOME}/.phylum/phylum."
+    else
+        check_copy "${bin_name}" "${HOME}/.phylum/phylum"
+    fi
+    chmod +x "${HOME}/.phylum/phylum"
 
-  # Copy the settings over, if settings do not already exist at the target.
-  if [ ! -f "${HOME}/.phylum/settings.yaml" ]; then
-      check_copy "settings.yaml" "${HOME}/.phylum/"
-  fi
+    # Copy the settings over, if settings do not already exist at the target.
+    if [ ! -f "${HOME}/.phylum/settings.yaml" ]; then
+        check_copy "settings.yaml" "${HOME}/.phylum/"
+    fi
+    chmod 600 "${HOME}/.phylum/settings.yaml"
 
-  # Copy completions over
-  mkdir -p "${HOME}/.phylum/completions"
-  cp -a "completions" "${HOME}/.phylum/"
-  success "Copied completions."
+    # Copy completions over
+    mkdir -p "${HOME}/.phylum/completions"
+    cp -a "completions" "${HOME}/.phylum/"
+    success "Copied completions."
 }
 
-pushd $(dirname "$0") >/dev/null
+pushd "$(dirname "$0")" >/dev/null
 banner
 create_directory
 copy_files
@@ -165,10 +156,9 @@ patch_zshrc
 popd >/dev/null
 
 success "Successfully installed phylum."
-cat << __instructions__ 
+cat << __instructions__
 
-   Source your $(get_rc_file) file, add $HOME/.phylum to your \$PATH variable, or
-   log in to a new terminal in order to make \`phylum\` available.
+    Source your $(get_rc_file) file, add $HOME/.phylum to your \$PATH variable, or
+    log in to a new terminal in order to make \`phylum\` available.
 
 __instructions__
-
