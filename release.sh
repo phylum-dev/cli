c5eb1010f66a703f04cc31b320b4fe0d5d468bef
Kyle Willmon <kyle@phylum.io>
2022-04-13 06:39:49 -0500

    Sign release tags and be descriptive (#253)

--- release.sh
+++ release.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+LATEST=$(git describe --tags --abbrev=0)
+
+printf "Latest release: %s\n\nGit log:\n" "${LATEST}"
+git log --oneline HEAD "^${LATEST}"
+printf "\n"
 
 printf "version: "
 read -r version
@@ -10,12 +16,12 @@ sed -E -i "1 s#^#* ${version} - ${changelog}\n#" CHANGELOG
 sed -E -i "s/^version = \"([^\"]*)\"/version = \"${version}\"/" cli/Cargo.toml
 git add CHANGELOG
 git add cli/Cargo.toml
-git commit -m "Bump version"
+git commit -m "v${version} - ${changelog}"
 
 TAG=v${version}
 
 echo Tagging / pushing "${TAG}", press any key to proceed...
 read -r
 git push
-git tag "${TAG}"
-git push --tags
+git tag --sign -m "${TAG} - ${changelog}" "${TAG}"
+git push "${TAG}"
