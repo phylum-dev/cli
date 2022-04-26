phylum-dev/cli

commit b383f0dbc8f80e3b5f1bc3a70855a6568f08c62f

Follow XDG directories spec (#251)
Previously phylum's installer would put all files into `~/.phylum` and
retrieve the configuration file from there. This leads to a rather
cluttered home directory which is why the XDG directories spec is
usually preferred.

The CLI will now check the XDG config directory first
(`${XDG_CONFIG_HOME:-${HOME}/.config}/phylum/settings.yaml`), then fall
back to the old config path if that isn't present.

The installer will put the completions in the XDG data directory
(`${XDG_DATA_HOME:-${HOME}/.local/share}/phylum`) and the binary in
`${HOME}/.local/bin/`.

Since the `${HOME}/.local/bin` path is sometimes included in the path
already, without being present in the shell config itself, we check the
current `$PATH` first to try and detect if we need to add it or not.

Closes #224.

cli/src/config.rs
@@ -1,6 +1,10 @@
 use std::env;
 use std::fs;
+#[cfg(unix)]
+use std::fs::Permissions;
 use std::io;
+#[cfg(unix)]
+use std::os::unix::fs::{OpenOptionsExt, PermissionsExt};
 use std::path::{Path, PathBuf};
 
 use anyhow::{anyhow, Result};
@@ -73,7 +77,6 @@ fn create_private_file<P: AsRef<Path>>(path: P) -> io::Result<fs::File> {
     opts.write(true).create(true).truncate(true);
     #[cfg(unix)]
     {
-        use std::os::unix::fs::OpenOptionsExt;
         opts.mode(0o600);
     }
 
@@ -160,7 +163,27 @@ pub fn get_home_settings_path() -> Result<PathBuf> {
     let home_path =
         home::home_dir().ok_or_else(|| anyhow!("Couldn't find the user's home directory"))?;
 
-    Ok(home_path.join(".phylum").join("settings.yaml"))
+    // Resolve XDG config directory.
+    let config_dir = env::var("XDG_CONFIG_DIR")
+        .ok()
+        .filter(|s| !s.is_empty())
+        .map(PathBuf::from)
+        .unwrap_or_else(|| home_path.join(".config"));
+
+    let config_path = config_dir.join("phylum").join("settings.yaml");
+    let old_config_path = home_path.join(".phylum").join("settings.yaml");
+
+    // Migrate the config from the old location.
+    if !config_path.exists() && old_config_path.exists() {
+        #[cfg(unix)]
+        {
+            fs::set_permissions(&old_config_path, Permissions::from_mode(0o600))?;
+        }
+        fs::create_dir_all(config_path.parent().unwrap())?;
+        fs::rename(old_config_path, &config_path).unwrap();
+    }
+
+    Ok(config_path)
 }
 
 #[cfg(test)]

cli/src/install.sh
@@ -7,6 +7,11 @@ NC='\033[0m'
 # Don't continue after failure:
 set -eu
 
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/phylum"
+data_dir="${XDG_DATA_HOME:-${HOME}/.local/share}/phylum"
+completions_dir="${data_dir}/completions"
+bin_dir="${HOME}/.local/bin"
+
 error() {
     printf "%b    ERROR%b %s\n" "${RED}" "${NC}" "${1}"
 }
@@ -48,59 +53,47 @@ get_rc_file() {
     esac
 }
 
-add_to_path_and_alias() {
-    rc_path=${1}
-    shell=${2}
-
-    # shellcheck disable=SC2016 # we don't want to expand $PATH here
-    if ! grep -q '.phylum:\$PATH' "${rc_path}"; then
-        export PATH="${HOME}/.phylum:${PATH}"
-        echo 'export PATH="$HOME/.phylum:$PATH"' >> "${rc_path}"
-        success "Updated ${shell}'s \$PATH to include ${HOME}/.phylum/."
-    fi
-
-    if ! grep -q 'alias ph=' "${rc_path}"; then
-        echo "alias ph='phylum'" >> "${rc_path}"
-        success "Created ph alias for phylum in ${shell}."
-    fi
-}
-
 patch_zshrc() {
+    phylum_rc="${data_dir}/zshrc"
     rc_path="${HOME}/.zshrc"
 
     if [ ! -f "${rc_path}" ]; then
         touch "${rc_path}"
     fi
 
-    if ! grep -q '.phylum/completions' "${rc_path}"; then
-        echo "fpath+=(\"\$HOME/.phylum/completions\")" >> "${rc_path}"
-    fi
-    if ! grep -q 'autoload -U compinit && compinit' "${rc_path}"; then
-        echo "autoload -U compinit && compinit" >> "${rc_path}"
-    fi
-    success "Completions are enabled for zsh."
+    echo "\
+export PATH=\"${bin_dir}:\$PATH\"
+alias ph='phylum'
+fpath+=(\"${completions_dir}\")
+autoload -U compinit && compinit" \
+    > "${phylum_rc}"
 
-    add_to_path_and_alias "${rc_path}" "zsh"
+    if ! grep -q "source ${phylum_rc}" "${rc_path}"; then
+        echo "source ${phylum_rc}" >> "${rc_path}"
+    fi
+
+    success "Completions are enabled for zsh."
 }
 
 patch_bashrc() {
+    phylum_rc="${data_dir}/bashrc"
     rc_path="${HOME}/.bashrc"
 
     if [ ! -f "${rc_path}" ]; then
         touch "${rc_path}"
     fi
 
-    if ! grep -q '.phylum/completions/phylum.bash' "${rc_path}"; then
-        echo "source \$HOME/.phylum/completions/phylum.bash" >> "${rc_path}"
+    echo "\
+export PATH=\"${bin_dir}:\$PATH\"
+alias ph='phylum'
+source ${completions_dir}/phylum.bash" \
+    > "${phylum_rc}"
+
+    if ! grep -q "source ${phylum_rc}" "${rc_path}"; then
+        echo "source ${phylum_rc}" >> "${rc_path}"
     fi
+
     success "Completions are enabled for bash."
-
-    add_to_path_and_alias "${rc_path}" "bash"
-}
-
-create_directory() {
-    # Create the config directory if one does not already exist.
-    install -d "${HOME}/.phylum"
 }
 
 copy_files() {
@@ -108,27 +101,44 @@ copy_files() {
     platform=$(set -e; get_platform)
     bin_name="phylum"
 
-    install -m 0755 "${bin_name}" "${HOME}/.phylum/phylum"
+    # Ensure binary directory exists.
+    mkdir -p "${bin_dir}"
+
+    install -m 0755 "${bin_name}" "${bin_dir}/${bin_name}"
     if [ "${platform}" = "macos" ]; then
         # Clear all extended attributes. The important one to remove here is 'com.apple.quarantine'
         # but there might be others or there might be none. `xattr -c` works in all of those cases.
-        xattr -c "${HOME}/.phylum/phylum"
-    fi
-
-    # Ensure correct permissions on settings.yaml (if it exists).
-    if [ -f "${HOME}/.phylum/settings.yaml" ]; then
-        chmod 600 "${HOME}/.phylum/settings.yaml"
+        xattr -c "${bin_dir}/${bin_name}"
     fi
 
     # Copy completions over
-    mkdir -p "${HOME}/.phylum/completions"
-    cp -a "completions" "${HOME}/.phylum/"
-    success "Copied completions to ${HOME}/.phylum/completions"
+    mkdir -p "${data_dir}"
+    cp -a "completions" "${data_dir}/"
+    success "Copied completions to ${completions_dir}"
+}
+
+# Remove old files and entries added before XDG directories conformity.
+cleanup_pre_xdg() {
+    # Remove old entries from bashrc.
+    sed -i'' "/^source \$HOME\/.phylum\/completions\/phylum.bash$/d" "${HOME}/.bashrc"
+    sed -i'' "/^export PATH=\"\$HOME\/.phylum:\$PATH\"$/d" "${HOME}/.bashrc"
+    sed -i'' "/^alias ph='phylum'$/d" "${HOME}/.bashrc"
+
+    # Remove old entries from bashrc.
+    sed -i'' "/^fpath+=(\"\$HOME\/.phylum\/completions\")$/d" "${HOME}/.zshrc"
+    sed -i'' "/^export PATH=\"\$HOME\/.phylum:\$PATH\"$/d" "${HOME}/.zshrc"
+    sed -i'' "/^alias ph='phylum'$/d" "${HOME}/.zshrc"
+
+    # Remove old phylum executable.
+    rm -f "${HOME}/.phylum/phylum"
+
+    # Remove old completions directory.
+    rm -rf "${HOME}/.phylum/completions"
 }
 
 cd "$(dirname "$0")"
 banner
-create_directory
+cleanup_pre_xdg
 copy_files
 patch_bashrc
 patch_zshrc
@@ -137,7 +147,7 @@ success "Successfully installed phylum."
 rc_file=$(get_rc_file)
 cat << __instructions__
 
-    Source your ${rc_file} file, add ${HOME}/.phylum to your \$PATH variable, or
+    Source your ${rc_file} file, add ${bin_dir} to your \$PATH variable, or
     log in to a new terminal in order to make \`phylum\` available.
 
 __instructions__

commit babbabbc686ff1d61a4b4b4e79f28b188bd2fe76

Fix XDG config directory variable
This fixes an issue introduced in b383f0d where `XDG_CONFIG_DIR` was
incorrectly used as the environment variable for overriding the config
directory according to the XDG spec.

The incorrect usage has been replaced with the correct
`XDG_CONFIG_HOME`.

cli/src/config.rs
@@ -169,7 +169,7 @@ pub fn get_home_settings_path() -> Result<PathBuf> {
         home::home_dir().ok_or_else(|| anyhow!("Couldn't find the user's home directory"))?;
 
     // Resolve XDG config directory.
-    let config_dir = env::var("XDG_CONFIG_DIR")
+    let config_dir = env::var("XDG_CONFIG_HOME")
         .ok()
         .filter(|s| !s.is_empty())
         .map(PathBuf::from)