211b99e3aab541d579abe931b021369202fad7fc
Andrea Venuta <andrea@phylum.io>
2022-05-23 18:47:23 +0200

    Injected dependencies and `deno_core::OpState` support

--- cli/src/commands/extensions/api.rs
+++ cli/src/commands/extensions/api.rs
@@ -1,3 +1,8 @@
+//! Most functions in this module are marked `#[allow(unused)]` for the time being. This
+//! intentional, as there are no clients for those functions in the rest of the code, but
+//! those functions will have to be used by the Deno integration; at that point, we may
+//! remove the annotations.
+
 use std::path::Path;
 use std::str::FromStr;
 
@@ -6,6 +11,8 @@ use crate::config::{get_current_project, Config};
 use crate::{api::PhylumApi, auth::UserInfo};
 
 use anyhow::{anyhow, Context, Error, Result};
+use deno_core::OpState;
+use once_cell::sync::Lazy;
 use phylum_types::types::auth::{AccessToken, RefreshToken};
 use phylum_types::types::common::{JobId, ProjectId};
 use phylum_types::types::job::JobStatusResponse;
@@ -14,13 +21,35 @@ use phylum_types::types::package::{
 };
 use phylum_types::types::project::ProjectDetailsResponse;
 
+/// Container for lazily evaluated dependencies of Extensions API functions. These values won't be
+/// visible from extension code and will have to be set up by the JS runtime builder which will
+/// load their configuration as any other command and then provide the pertinent factories.
+struct InjectedDependencies {
+    api: Lazy<PhylumApi>,
+    config: Lazy<Config>,
+}
+
+impl InjectedDependencies {
+    #[allow(unused)]
+    pub(super) fn from_factories(
+        api_factory: fn() -> PhylumApi,
+        config_factory: fn() -> Config,
+    ) -> Self {
+        InjectedDependencies {
+            api: Lazy::new(api_factory),
+            config: Lazy::new(config_factory),
+        }
+    }
+}
+
 #[allow(unused)]
 pub(super) async fn phylum_analyze(
-    api: &mut PhylumApi,
+    state: &mut OpState,
     lockfile: &str,
     project: Option<&str>,
     group: Option<&str>,
 ) -> Result<ProjectId> {
+    let api = &mut state.borrow_mut::<InjectedDependencies>().api;
     let (packages, request_type) = get_packages_from_lockfile(Path::new(lockfile))
         .context("Unable to locate any valid package in package lockfile")?;
 
@@ -50,16 +79,21 @@ pub(super) async fn phylum_analyze(
 }
 
 #[allow(unused)]
-pub(super) async fn phylum_auth_status(api: &mut PhylumApi, config: &Config) -> Result<UserInfo> {
-    api.user_info(&config.auth_info).await.map_err(Error::from)
+pub(super) async fn phylum_auth_status(state: &mut OpState) -> Result<UserInfo> {
+    let deps = state.borrow_mut::<InjectedDependencies>();
+    deps.api
+        .user_info(&deps.config.auth_info)
+        .await
+        .map_err(Error::from)
 }
 
 #[allow(unused)]
 pub(super) async fn phylum_auth_token_bearer(
-    config: &Config,
+    state: &mut OpState,
     ignore_certs: bool,
 ) -> Result<AccessToken> {
-    let refresh_token = phylum_auth_token(config)?;
+    let refresh_token = phylum_auth_token(state)?;
+    let config = &mut state.borrow_mut::<InjectedDependencies>().config;
     let access_token =
         crate::auth::handle_refresh_tokens(&config.auth_info, &refresh_token, ignore_certs)
             .await?
@@ -68,7 +102,8 @@ pub(super) async fn phylum_auth_token_bearer(
 }
 
 #[allow(unused)]
-pub(super) fn phylum_auth_token(config: &Config) -> Result<RefreshToken> {
+pub(super) fn phylum_auth_token(state: &mut OpState) -> Result<RefreshToken> {
+    let config = &mut state.borrow_mut::<InjectedDependencies>().config;
     config
         .auth_info
         .offline_access
@@ -78,9 +113,10 @@ pub(super) fn phylum_auth_token(config: &Config) -> Result<RefreshToken> {
 
 #[allow(unused)]
 pub(super) async fn phylum_history_job(
-    api: &mut PhylumApi,
+    state: &mut OpState,
     job_id: Option<&str>,
 ) -> Result<JobStatusResponse<PackageStatusExtended>> {
+    let api = &mut state.borrow_mut::<InjectedDependencies>().api;
     let job_id = job_id
         .map(|job_id| JobId::from_str(job_id).ok())
         .unwrap_or_else(|| get_current_project().map(|p| p.id))
@@ -90,9 +126,10 @@ pub(super) async fn phylum_history_job(
 
 #[allow(unused)]
 pub(super) async fn phylum_history_project(
-    api: &mut PhylumApi,
+    state: &mut OpState,
     project_name: Option<&str>,
 ) -> Result<ProjectDetailsResponse> {
+    let api = &mut state.borrow_mut::<InjectedDependencies>().api;
     let project_name = project_name
         .map(String::from)
         .map(Result::Ok)
@@ -108,11 +145,12 @@ pub(super) async fn phylum_history_project(
 
 #[allow(unused)]
 pub(super) async fn phylum_package(
-    api: &mut PhylumApi,
+    state: &mut OpState,
     name: &str,
     version: &str,
     package_type: &str,
 ) -> Result<Package> {
+    let api = &mut state.borrow_mut::<InjectedDependencies>().api;
     let package_type = PackageType::from_str(package_type)
         .map_err(|e| anyhow!("Unrecognized package type `{package_type}`: {e:?}"))?;
     api.get_package_details(&PackageDescriptor {
