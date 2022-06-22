2ada9cff16e020a7b6020a18b61388e161fa8ecd
Christian Dürr <102963075+cd-work@users.noreply.github.com>
2022-06-22 17:23:14 +0000

    Add support for effective-pom.xml workspaces (#493)
    Fixes #469.

--- cli/src/lockfiles/java.rs
+++ cli/src/lockfiles/java.rs
@@ -3,6 +3,7 @@ use nom::error::convert_error;
 use nom::Finish;
 use phylum_types::ecosystems::maven::{Dependency, Plugin, Project};
 use phylum_types::types::package::{PackageDescriptor, PackageType};
+use serde::Deserialize;
 
 use super::parsers::gradle_dep;
 use crate::lockfiles::{Parse, ParseResult};
@@ -25,55 +26,32 @@ impl Parse for GradleLock {
     }
 }
 
-impl Parse for Pom {
-    /// Parses maven effecti-pom files into a vec of packages
-    fn parse(&self, data: &str) -> ParseResult {
-        // Get plugin dependencies
-        fn get_plugin_deps(plugins: &[Plugin]) -> Vec<Dependency> {
-            plugins
-                .iter()
-                .flat_map(|plugin| {
-                    let mut deps = plugin.dependencies.clone().unwrap_or_default().dependencies;
-                    deps.push(Dependency {
-                        group_id: plugin.group_id.clone(),
-                        artifact_id: plugin.artifact_id.clone(),
-                        version: plugin.version.clone(),
-                        dtype: None,
-                        classifier: None,
-                        scope: None,
-                        system_path: None,
-                        exclusions: None,
-                        optional: None,
-                    });
-                    deps
-                })
-                .collect::<Vec<_>>()
-        }
-
-        // Get project reference
-        let pom: Project = serde_xml_rs::from_str(data)?;
-
+impl Pom {
+    /// Get dependencies of a single project.
+    fn project_dependencies(&self, project: Project) -> ParseResult {
         // Get project dependencies
-        let mut dependencies = pom.dependencies.unwrap_or_default().dependencies;
+        let mut dependencies = project.dependencies.unwrap_or_default().dependencies;
 
         // Get the reporting dependencies
-        let mut reporting_dependencies = get_plugin_deps(
-            &pom.reporting
+        let mut reporting_dependencies = Self::get_plugin_deps(
+            &project
+                .reporting
                 .unwrap_or_default()
                 .plugins
                 .unwrap_or_default(),
         );
 
         // Combine plugins and plugin dependencies
-        let mut build_plugins = get_plugin_deps(
-            &pom.build
+        let mut build_plugins = Self::get_plugin_deps(
+            &project
+                .build
                 .as_ref()
                 .and_then(|b| b.plugins.clone())
                 .unwrap_or_default(),
         );
 
         // Get build artifacts
-        let build_ext = &pom
+        let build_ext = &project
             .build
             .unwrap_or_default()
             .extensions
@@ -92,15 +70,16 @@ impl Parse for Pom {
             })
             .collect::<Vec<_>>();
 
-        let mut profile_dependencies = pom
+        let mut profile_dependencies = project
             .profiles
             .unwrap_or_default()
             .profiles
             .into_iter()
             .flat_map(|p| {
                 let mut p_deps = p.dependencies.unwrap_or_default().dependencies;
-                let p_report_plugins =
-                    get_plugin_deps(&p.reporting.unwrap_or_default().plugins.unwrap_or_default());
+                let p_report_plugins = Self::get_plugin_deps(
+                    &p.reporting.unwrap_or_default().plugins.unwrap_or_default(),
+                );
                 p_deps.extend(p_report_plugins);
                 p_deps
             })
@@ -127,6 +106,68 @@ impl Parse for Pom {
                 })
             })
             .collect::<Result<Vec<_>, _>>()
+    }
+
+    /// Get plugin dependencies.
+    fn get_plugin_deps(plugins: &[Plugin]) -> Vec<Dependency> {
+        plugins
+            .iter()
+            .flat_map(|plugin| {
+                let mut deps = plugin.dependencies.clone().unwrap_or_default().dependencies;
+                deps.push(Dependency {
+                    group_id: plugin.group_id.clone(),
+                    artifact_id: plugin.artifact_id.clone(),
+                    version: plugin.version.clone(),
+                    dtype: None,
+                    classifier: None,
+                    scope: None,
+                    system_path: None,
+                    exclusions: None,
+                    optional: None,
+                });
+                deps
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
+#[derive(Deserialize)]
+enum EffectivePom {
+    #[serde(rename = "project")]
+    Project(Box<Project>),
+    #[serde(rename = "projects")]
+    Workspace(Workspace),
+}
+
+#[derive(Deserialize)]
+struct Workspace {
+    #[serde(rename = "project")]
+    projects: Vec<Project>,
+}
+
+impl Parse for Pom {
+    /// Parses maven effecti-pom files into a vec of packages
+    fn parse(&self, data: &str) -> ParseResult {
+        // Parse effective-pom.xml.
+        let pom: EffectivePom = serde_xml_rs::from_str(data)?;
+
+        // Retrieve all dependencies.
+        match pom {
+            EffectivePom::Project(project) => self.project_dependencies(*project),
+            EffectivePom::Workspace(workspace) => {
+                // Retrieve all dependencies.
+                let mut packages = Vec::new();
+                for project in workspace.projects {
+                    packages.append(&mut self.project_dependencies(project)?);
+                }
+
+                // Deduplicate between projects.
+                packages.sort_unstable();
+                packages.dedup();
+
+                Ok(packages)
+            }
+        }
     }
 
     fn package_type(&self) -> PackageType {
@@ -173,5 +214,22 @@ mod tests {
         assert_eq!(last.name, "org.apache.maven.plugins:maven-site-plugin");
         assert_eq!(last.version, "3.3");
         assert_eq!(last.package_type, PackageType::Maven);
+    }
+
+    #[test]
+    fn lock_parse_workspace_effective_pom() {
+        let pkgs = Pom
+            .parse_file("tests/fixtures/workspace-effective-pom.xml")
+            .unwrap();
+
+        assert_eq!(pkgs.len(), 17);
+
+        let additional_dependency = PackageDescriptor {
+            name: "io.phylum:fake-dependency".into(),
+            version: "1.2.3".into(),
+            package_type: PackageType::Maven,
+        };
+
+        assert!(pkgs.contains(&additional_dependency));
     }
 }
