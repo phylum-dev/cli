00b459453739bdd206d81f18284da2c8404dabb1
Andrea Venuta <andrea@phylum.io>
2022-06-24 18:35:22 +0200

    Removed trailing newlines

--- cli/tests/fixtures/extensions/permissions/correct-read-perms/main.ts
+++ cli/tests/fixtures/extensions/permissions/correct-read-perms/main.ts
@@ -2,4 +2,3 @@ import { walk, exists } from "https://deno.land/std@0.143.0/fs/mod.ts"
 
 let data = new TextDecoder().decode(await Deno.readFile("./correct-read-perms/main.ts"))
 console.log(data)
-
