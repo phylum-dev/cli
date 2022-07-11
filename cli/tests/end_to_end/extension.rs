2c3cbe934fb3b7cfad40ee36e8327e72af278da4
andreaphylum <68552656+andreaphylum@users.noreply.github.com>
2022-07-11 17:30:59 +0200

    Split end-to-end tests in multiple methods (#542)

--- cli/tests/end_to_end/extension.rs
+++ cli/tests/end_to_end/extension.rs
@@ -2,77 +2,96 @@ use crate::common::{create_lockfile, create_project, TestCli};
 
 /// Test Phylum API functions.
 #[tokio::test]
-pub async fn api() {
+pub async fn get_user_info() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    test_cli
+        .create_extension("console.log(await PhylumApi.getUserInfo())")
+        .run()
+        .success()
+        .stdout(predicates::str::contains("email"));
+}
+
+#[tokio::test]
+pub async fn get_access_token() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    test_cli
+        .create_extension("console.log(await PhylumApi.getAccessToken())")
+        .run()
+        .success()
+        .stdout(predicates::str::contains("ey"));
+}
+
+#[tokio::test]
+pub async fn get_refresh_token() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    test_cli
+        .create_extension("console.log(await PhylumApi.getRefreshToken())")
+        .run()
+        .success()
+        .stdout(predicates::str::contains("ey"));
+}
+
+#[tokio::test]
+pub async fn get_package_details() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    test_cli
+        .create_extension(
+            "console.log(await PhylumApi.getPackageDetails('express', '4.18.1', 'npm'))",
+        )
+        .run()
+        .success()
+        .stdout(predicates::str::contains("vulnerability: 1"));
+}
+
+#[tokio::test]
+pub async fn get_project_details() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    let project = create_project().await;
+
+    let project_details = format!("console.log(await PhylumApi.getProjectDetails({project:?}))");
+    test_cli
+        .create_extension(&project_details)
+        .run()
+        .success()
+        .stdout(predicates::str::contains(r#"name: "integration-tests""#));
+}
+
+#[tokio::test]
+pub async fn parse_lockfile() {
+    let test_cli = TestCli::builder().with_config(true).build();
+
+    let lockfile = create_lockfile(test_cli.temp_path());
+
+    let parse_lockfile = format!(
+        "const packages = await PhylumApi.parseLockfile({lockfile:?}, 'yarn');
+             console.log(packages);",
+    );
+    test_cli.create_extension(&parse_lockfile).run().success().stdout(predicates::str::contains(
+        r#"[ { name: "accepts", version: "1.3.8", type: "npm" } ]"#,
+    ));
+}
+
+#[tokio::test]
+pub async fn get_job_status() {
     let test_cli = TestCli::builder().with_config(true).build();
 
     let lockfile = create_lockfile(test_cli.temp_path());
     let project = create_project().await;
 
-    {
-        test_cli
-            .create_extension("console.log(await PhylumApi.getUserInfo())")
-            .run()
-            .success()
-            .stdout(predicates::str::contains("email"));
-    }
-
-    {
-        test_cli
-            .create_extension("console.log(await PhylumApi.getAccessToken())")
-            .run()
-            .success()
-            .stdout(predicates::str::contains("ey"));
-    }
-
-    {
-        test_cli
-            .create_extension("console.log(await PhylumApi.getRefreshToken())")
-            .run()
-            .success()
-            .stdout(predicates::str::contains("ey"));
-    }
-
-    {
-        test_cli
-            .create_extension(
-                "console.log(await PhylumApi.getPackageDetails('express', '4.18.1', 'npm'))",
-            )
-            .run()
-            .success()
-            .stdout(predicates::str::contains("vulnerability: 1"));
-    }
-
-    {
-        let project_details =
-            format!("console.log(await PhylumApi.getProjectDetails({project:?}))");
-        test_cli
-            .create_extension(&project_details)
-            .run()
-            .success()
-            .stdout(predicates::str::contains(r#"name: "integration-tests""#));
-    }
-
-    {
-        let parse_lockfile = format!(
-            "const packages = await PhylumApi.parseLockfile({lockfile:?}, 'yarn');
-             console.log(packages);",
-        );
-        test_cli.create_extension(&parse_lockfile).run().success().stdout(
-            predicates::str::contains(r#"[ { name: "accepts", version: "1.3.8", type: "npm" } ]"#),
-        );
-    }
-
-    {
-        let analyze = format!(
-            "const jobId = await PhylumApi.analyze({lockfile:?}, {project:?});
+    let analyze = format!(
+        "const jobId = await PhylumApi.analyze({lockfile:?}, {project:?});
              console.log(await PhylumApi.getJobStatus(jobId));"
-        );
-        test_cli
-            .create_extension(&analyze)
-            .run()
-            .success()
-            .stdout(predicates::str::contains(r#"name: "accepts""#));
-    }
+    );
+    test_cli
+        .create_extension(&analyze)
+        .run()
+        .success()
+        .stdout(predicates::str::contains(r#"name: "accepts""#));
 }
 
 /// Ensure shared state is async and thread safe.
