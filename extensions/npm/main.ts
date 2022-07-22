7539e9a346bd90edbbcd91fca857d994c6619b60
Christian Duerr <chris.durr@phylum.io>
2022-07-22 23:56:46 +0200

    Update NPM extension with passthrough
    This adds pathrough of commands other than `npm install` to the `phylum
    npm` extension.

--- extensions/npm/main.ts
+++ extensions/npm/main.ts
@@ -1,17 +1,18 @@
 import { PhylumApi } from "phylum";
 
-// Parse CLI args.
-const args = Deno.args;
-if (args.length != 2 || args[0] !== "install") {
-    console.error("Usage: phylum npm install <PKG>");
+if (Deno.args.length >= 1 && 'install'.startsWith(Deno.args[0])) {
+    // Analyze new dependencies with phylum before install.
+    await install(Deno.args);
 } else {
-    await install(args[1]);
+  let status = await Deno.run({ cmd: ['npm', ...Deno.args] }).status()
+
+  Deno.exit(status.code)
 }
 
 // Analyze and install package.
-async function install(pkg: string) {
+async function install(args: string[]) {
     console.log("Updating package lock…");
-    await Deno.run({ cmd: ["npm", "i", "--package-lock-only", pkg] }).status();
+    await Deno.run({ cmd: ["npm", "i", "--package-lock-only", ...args] }).status();
     console.log("Package lock updated.\n");
 
     console.log("Analyzing packages…");
@@ -22,11 +23,13 @@ async function install(pkg: string) {
         console.log("All packages pass project thresholds.\n");
 
         console.log(`Installing '${pkg}'…`);
-        await Deno.run({ cmd: ["npm", "i", pkg] }).status();
+        await Deno.run({ cmd: ["npm", "i", ...args] }).status();
         console.log("Package install complete.");
     } else if (jobStatus.pass) {
         console.warn("Unknown packages were submitted for analysis, please check again later.");
+        Deno.exit(9990);
     } else {
         console.error(`Installing '${pkg}' caused threshold failure.`);
+        Deno.exit(9991);
     }
 }
