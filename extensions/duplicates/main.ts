fd36d227eae9f12d68e0ecdea416978aa3b86f79
Christian Dürr <102963075+cd-work@users.noreply.github.com>
2022-08-03 18:07:45 +0000

    Update extension example documentation (#585)
    This updates the existing extension example documentation to take the
    new permission system into account.
    
    It also updates to the latest `parseLockfile` return type.
    
    Closes #580.

--- extensions/duplicates/main.ts
+++ extensions/duplicates/main.ts
@@ -1,38 +1,30 @@
-import { mapValues } from "https://deno.land/std@0.146.0/collections/map_values.ts";
-import { distinct } from "https://deno.land/std@0.146.0/collections/distinct.ts";
-import { groupBy } from "https://deno.land/std@0.146.0/collections/group_by.ts";
+import { mapValues } from "https://deno.land/std@0.150.0/collections/map_values.ts";
+import { distinct } from "https://deno.land/std@0.150.0/collections/distinct.ts";
+import { groupBy } from "https://deno.land/std@0.150.0/collections/group_by.ts";
 
 import { PhylumApi } from "phylum";
 
 // Ensure lockfile argument is present.
 if (Deno.args.length != 1) {
     console.error("Usage: phylum duplicates <LOCKFILE>");
-} else {
-    // Use first CLI parameter as our lockfile.
-    const lockfile = Deno.args[0];
+    Deno.exit(1);
+}
 
-    // Parse lockfile using Phylum's API.
-    const deps = await PhylumApi.parseLockfile(lockfile);
+// Parse lockfile using Phylum's API.
+const lockfile = await PhylumApi.parseLockfile(Deno.args[0]);
 
-    // Group all versions for the same dependency together.
-    const groupedDeps = groupBy(deps, dep => dep.name);
+// Group all versions for the same dependency together.
+const groupedDeps = groupBy(lockfile.packages, dep => dep.name);
 
-    // Reduce each dependency to a list of its versions.
-    const reducedDeps = mapValues(groupedDeps, deps => deps.map(dep => dep.version));
+// Reduce each dependency to a list of its versions.
+const reducedDeps = mapValues(groupedDeps, deps => deps.map(dep => dep.version));
 
-    for (const [dep, versions] of Object.entries(reducedDeps)) {
-        // Deduplicate identical versions.
-        const distinctVersions = distinct(versions);
+for (const [dep, versions] of Object.entries(reducedDeps)) {
+    // Deduplicate identical versions.
+    const distinctVersions = distinct(versions);
 
-        // Print all dependencies with more than one version.
-        if (distinctVersions.length > 1) {
-            console.log(`${dep}:`, distinctVersions);
-
-            // await Deno.writeTextFile(
-            //     "./duplicates.txt",
-            //     `${dep}: ${distinctVersions}\n`,
-            //     { append: true }
-            // );
-        }
+    // Print all dependencies with more than one version.
+    if (distinctVersions.length > 1) {
+        console.log(`${dep}:`, distinctVersions);
     }
 }
