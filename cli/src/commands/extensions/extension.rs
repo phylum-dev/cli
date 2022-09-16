3b7ff9484d3d55fbc7cf4ff27c2d1a614f4b51c7
Christian Dürr <102963075+cd-work@users.noreply.github.com>
2022-09-16 15:07:16 +0000

    Update cli/src/commands/extensions/extension.rs
    Co-authored-by: andreaphylum <68552656+andreaphylum@users.noreply.github.com>

--- cli/src/commands/extensions/extension.rs
+++ cli/src/commands/extensions/extension.rs
@@ -207,7 +207,7 @@ pub fn validate_name(name: &str) -> Result<(), anyhow::Error> {
     if EXTENSION_NAME_RE.is_match(name) {
         Ok(())
     } else {
-        Err(anyhow!("{}: invalid extension name, must be lowercase alphanumeric, dash (-)", name))
+        Err(anyhow!("{}: invalid extension name, must start with a letter and can contain only lowercase alphanumeric character or dashes (-)", name))
     }
 }
 
