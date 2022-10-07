3ae1a396d1706187ea592528399c1961cfce5ec5
ein-tier <westley@phylum.io>
2022-10-07 17:14:10 +0200

    rework go_sum parser to error on unexpected data

--- lockfile/src/parsers/go_sum.rs
+++ lockfile/src/parsers/go_sum.rs
@@ -3,30 +3,34 @@ use std::ops::Not;
 use nom::bytes::complete::take_until;
 use nom::character::complete::space0;
 use nom::combinator::recognize;
+use nom::multi::many1;
 use phylum_types::types::package::{PackageDescriptor, PackageType};
 
-use super::Result;
+use super::{take_till_line_end, Result};
 
 pub fn parse(input: &str) -> Result<&str, Vec<PackageDescriptor>> {
-    let pkgs = input.lines().filter_map(package).collect::<Vec<_>>();
+    let (input, pkg_options) = many1(package)(input)?;
+    let pkgs = pkg_options.iter().flatten().cloned().collect::<Vec<_>>();
+
     Ok((input, pkgs))
 }
 
-fn package(input: &str) -> Option<PackageDescriptor> {
-    let (input, name) = package_name(input).ok()?;
-    let (input, version) = package_version(input).ok()?;
-    let (_, _hash) = package_hash(input).ok()?;
+fn package(input: &str) -> Result<&str, Option<PackageDescriptor>> {
+    let (input, name) = package_name(input)?;
+    let (input, version) = package_version(input)?;
+    let (input, _hash) = package_hash(input)?;
 
     // If the package version ends in "/go.mod" then this entry
     // just records the hash for a go.mod file. The package this
     // go.mod file belongs to will also be listed, and that's what
     // we're interested in, so we simply discard this entry.
-
-    version.ends_with(r"/go.mod").not().then(|| PackageDescriptor {
+    let package = version.ends_with(r"/go.mod").not().then(|| PackageDescriptor {
         name: name.to_string(),
         version: version.to_string(),
         package_type: PackageType::Golang,
-    })
+    });
+
+    Ok((input, package))
 }
 
 fn package_name(input: &str) -> Result<&str, &str> {
@@ -46,8 +50,8 @@ fn package_version(input: &str) -> Result<&str, &str> {
 }
 
 fn package_hash(input: &str) -> Result<&str, &str> {
-    // take away the leading whitespace, then the hash is
-    // everything left
-    let (hash, _) = space0(input)?;
-    Ok(("", hash))
+    // take away the leading whitespace, then the hash
+    // is everything left until the end of the line
+    let (input, _) = space0(input)?;
+    take_till_line_end(input)
 }
