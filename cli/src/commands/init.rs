a33416ec23a6e2093a17b4a664941118e0687a03
Christian Dürr <102963075+cd-work@users.noreply.github.com>
2022-12-05 22:29:57 +0000

    Ignore parent directory projects for `phylum init` (#840)
    Previously, `phylum init` would query if the project should be
    overwritten when a parent project contained a project file already.
    
    Since we do not actually overwrite any project files outside of the
    current directory, and since having nested Phylum projects is perfectly
    valid, a warning is now only emitted when there's already a Phylum
    project file in the current directory.
    
    Closes #839.

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -16,8 +16,8 @@ use crate::{config, print_user_success, print_user_warning};
 
 /// Handle `phylum init` subcommand.
 pub async fn handle_init(api: &mut PhylumApi, matches: &ArgMatches) -> CommandResult {
-    // Prompt for confirmation if there already is a linked project.
-    if !matches.get_flag("force") && config::get_current_project().is_some() {
+    // Prompt for confirmation if a linked project is already in this directory.
+    if !matches.get_flag("force") && config::find_project_conf(".", false).is_some() {
         print_user_warning!("Workspace is already linked to a Phylum project");
         let should_continue = Confirm::new()
             .with_prompt("Overwrite existing project configuration?")
