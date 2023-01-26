bd8f82406463ff0bca466c811189c3ae791b1537
Christian Duerr <chris.durr@phylum.io>
2023-01-26 18:47:03 +0100

    Fix clippy warnings

--- cli/src/format.rs
+++ cli/src/format.rs
@@ -317,7 +317,7 @@ where
         .ecosystems
         .iter()
         .flat_map(|ecosystem| {
-            Some(match PackageType::from_str(&ecosystem).ok()? {
+            Some(match PackageType::from_str(ecosystem).ok()? {
                 PackageType::Npm => "NPM",
                 PackageType::RubyGems => "RubyGems",
                 PackageType::PyPi => "PyPI",
@@ -359,7 +359,7 @@ where
     let mut summary = details.iter().fold("".to_string(), |acc, x| {
         format!("{}\n{:>16}: {:<36} {:>24}: {:<36}", acc, x.0, x.1, x.2, x.3)
     });
-    summary = format!("{}\n{:>16}: {}", summary, ecosystems_title, ecosystems_value);
+    summary = format!("{summary}\n{ecosystems_title:>16}: {ecosystems_value}");
 
     let status = if resp.num_incomplete > 0 {
         format!("{:>16}: {}", "Status", style("INCOMPLETE").yellow())
