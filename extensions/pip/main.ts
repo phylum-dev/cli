4e287a34bb9f8c22f110761ff353bb82fcd6a821
Kyle Willmon <kyle@phylum.io>
2023-03-23 15:43:54 -0500

    deno fmt pip/main.ts

--- extensions/pip/main.ts
+++ extensions/pip/main.ts
@@ -32,7 +32,11 @@ const knownSubcommands = [
 // the first subcommand (i.e.: `pip --no-color install package`).
 const subcommand = Deno.args[0];
 if (Deno.args.length != 0 && !knownSubcommands.includes(subcommand)) {
-  console.error(`[${red("phylum")}] This extension does not support arguments before the first subcommand. Please open an issue if "${subcommand}" is not an argument.`);
+  console.error(
+    `[${
+      red("phylum")
+    }] This extension does not support arguments before the first subcommand. Please open an issue if "${subcommand}" is not an argument.`,
+  );
   Deno.exit(125);
 }
 
@@ -52,8 +56,25 @@ const installStatus = PhylumApi.runSandboxed({
   args: Deno.args,
   exceptions: {
     run: ["./", "/bin", "/usr/bin", "/usr/local/bin", "~/.pyenv"],
-    write: ["./", "~/Library/Caches", "~/Library/Python", "~/.cache", "~/.local", "~/.pyenv", "/tmp"],
-    read: ["~/Library/Caches", "~/Library/Python", "~/.cache", "~/.local", "~/.pyenv", "/tmp", "/etc/passwd", "/etc/apache2/mime.types"],
+    write: [
+      "./",
+      "~/Library/Caches",
+      "~/Library/Python",
+      "~/.cache",
+      "~/.local",
+      "~/.pyenv",
+      "/tmp",
+    ],
+    read: [
+      "~/Library/Caches",
+      "~/Library/Python",
+      "~/.cache",
+      "~/.local",
+      "~/.pyenv",
+      "/tmp",
+      "/etc/passwd",
+      "/etc/apache2/mime.types",
+    ],
     net: true,
   },
 });
@@ -64,27 +85,27 @@ type JobStatus = {
     issues: {
       severity: string;
       title: string;
-    }[]
-  }[]
+    }[];
+  }[];
 };
 
 // Logs any identified issues to the screen.
 function logIssues(jobStatus: Record<string, unknown>) {
-  for(const pkg of (jobStatus as JobStatus).packages) {
+  for (const pkg of (jobStatus as JobStatus).packages) {
     const issues = pkg.issues;
 
-    for(const issue of issues) {
+    for (const issue of issues) {
       let severity = issue.severity.toLowerCase();
-      
-      if(["high", "critical"].indexOf(severity) != -1) {
+
+      if (["high", "critical"].indexOf(severity) != -1) {
         severity = red(severity);
-      } else if(severity == "medium") {
+      } else if (severity == "medium") {
         severity = yellow(severity);
       } else {
         severity = green(severity);
       }
 
-      console.log(`    [${severity}] ${issue.title}`)
+      console.log(`    [${severity}] ${issue.title}`);
     }
   }
 }
@@ -98,19 +119,32 @@ async function checkDryRun() {
     args: [...Deno.args, "--dry-run"],
     exceptions: {
       run: ["./", "/bin", "/usr/bin", "/usr/local/bin", "~/.pyenv"],
-      write: ["~/Library/Caches", "~/.pyenv", "~/.cache", "~/.local/lib", "/tmp"],
-      read: ["~/Library/Caches", "~/.cache", "~/.local/lib", "/tmp", "/etc/passwd", "/etc/apache2/mime.types"],
+      write: [
+        "~/Library/Caches",
+        "~/.pyenv",
+        "~/.cache",
+        "~/.local/lib",
+        "/tmp",
+      ],
+      read: [
+        "~/Library/Caches",
+        "~/.cache",
+        "~/.local/lib",
+        "/tmp",
+        "/etc/passwd",
+        "/etc/apache2/mime.types",
+      ],
       net: true,
     },
-    stdout: 'piped',
-    stderr: 'piped',
+    stdout: "piped",
+    stderr: "piped",
   });
 
   // Ensure dry-run was successful.
   if (!status.success) {
     console.error(`[${red("phylum")}] Pip dry-run failed.\n`);
     Deno.exit(status.code ?? 255);
-  } 
+  }
 
   // Parse dry-run output.
   const packages = parseDryRun(status.stdout);
@@ -157,10 +191,10 @@ type Package = {
 function parseDryRun(output: string): Package[] {
   // Extract the "Would install [..]" line.
   let new_deps;
-  const lines = output.split('\n');
+  const lines = output.split("\n");
   for (const line of lines) {
-    if (line.startsWith('Would install ')) {
-      new_deps = line.substring('Would install '.length);
+    if (line.startsWith("Would install ")) {
+      new_deps = line.substring("Would install ".length);
       break;
     }
   }
@@ -174,12 +208,12 @@ function parseDryRun(output: string): Package[] {
   const packages: Package[] = [];
 
   // Parse dependency names and versions.
-  const deps = new_deps.split(' ');
+  const deps = new_deps.split(" ");
   for (let i = 0; i < deps.length; i++) {
     const pkg = deps[i];
-    const lastDashIndex = pkg.lastIndexOf('-');
+    const lastDashIndex = pkg.lastIndexOf("-");
     const pkgName = pkg.substring(0, lastDashIndex);
-    const pkgVer = pkg.substring(lastDashIndex+1);
+    const pkgVer = pkg.substring(lastDashIndex + 1);
 
     packages.push({
       name: pkgName,
