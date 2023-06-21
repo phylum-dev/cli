b78793e2e12e5f1071c391d5879b70ff2ef94745
Christian Dürr <102963075+cd-work@users.noreply.github.com>
2023-06-21 20:24:49 +0000

    Fix line continuations and --hash in python parser (#1126)
    This fixes the handling of line continuations in the requirements.txt
    file.
    
    It also resolves an issue where the `--hash` argument was not accepted
    by the lockfile parser and caused the parsing of the file to fail.
    
    Closes #1117.

--- lockfile/src/parsers/mod.rs
+++ lockfile/src/parsers/mod.rs
@@ -1,10 +1,10 @@
 use nom::branch::alt;
-use nom::bytes::complete::{tag, take, take_until};
-use nom::character::complete::{line_ending, multispace0, space0};
-use nom::combinator::{eof, opt, recognize};
+use nom::bytes::complete::{tag, take_until};
+use nom::character::complete::{line_ending, multispace0, not_line_ending, space0};
+use nom::combinator::{eof, opt, recognize, rest};
 use nom::error::{context, VerboseError};
 use nom::multi::{count, many1, many_till};
-use nom::sequence::{delimited, tuple};
+use nom::sequence::{delimited, terminated, tuple};
 use nom::{AsChar, IResult};
 
 pub mod gem;
@@ -14,12 +14,33 @@ pub mod pypi;
 pub mod spdx;
 pub mod yarn;
 
+/// Consume everything until the next `\n` or `\r\n`.
 fn take_till_line_end(input: &str) -> Result<&str, &str> {
-    recognize(tuple((alt((take_until("\n"), take_until("\r\n"))), take(1usize))))(input)
+    recognize(terminated(not_line_ending, line_ending))(input)
 }
 
+/// Consume everything until the next `\n\n` or `\r\n\r\n`.
 fn take_till_blank_line(input: &str) -> Result<&str, &str> {
     recognize(alt((take_until("\n\n"), take_until("\r\n\r\n"))))(input)
+}
+
+/// Consume the next line.
+///
+/// This supports both `\n` and `\r\n`. It also skips line continuations
+/// (`\\\n`, `\\\r\n`) and stops on EOF.
+fn take_continued_line(mut input: &str) -> Result<&str, ()> {
+    loop {
+        // Get everything up to the next NL or EOF.
+        let (new_input, line) = recognize(alt((take_till_line_end, rest)))(input)?;
+        input = new_input;
+
+        // Stop consuming lines once there are no continuations.
+        if !line.ends_with("\\\n") && !line.ends_with("\\\r\n") {
+            break;
+        }
+    }
+
+    Ok((input, ()))
 }
 
 type Result<T, U> = IResult<T, U, VerboseError<T>>;
--- lockfile/src/parsers/pypi.rs
+++ lockfile/src/parsers/pypi.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use nom::branch::alt;
 use nom::bytes::complete::{tag, take_till, take_until};
-use nom::character::complete::{alphanumeric1, char, not_line_ending, space0};
+use nom::character::complete::{alphanumeric1, char, line_ending, space1};
 use nom::combinator::{eof, opt, recognize, rest, verify};
 use nom::error::{VerboseError, VerboseErrorKind};
 use nom::multi::{many0, many1, separated_list0};
@@ -10,18 +10,26 @@ use nom::sequence::{delimited, pair, terminated};
 use nom::Err as NomErr;
 use phylum_types::types::package::PackageType;
 
-use crate::parsers::Result;
+use crate::parsers::{self, Result};
 use crate::{Package, PackageVersion};
 
-pub fn parse(input: &str) -> Result<&str, Vec<Package>> {
+pub fn parse(mut input: &str) -> Result<&str, Vec<Package>> {
     let mut pkgs = Vec::new();
 
-    for line in input
-        .lines()
-        .map(|line| line.trim())
-        .filter(|line| !line.starts_with('#') && !line.is_empty())
-    {
-        let (_, line) = recognize(alt((take_until(" #"), not_line_ending)))(line)?;
+    while !input.is_empty() {
+        // Get the next line.
+        let (new_input, line) = line(input)?;
+        input = new_input;
+
+        // Ignore empty lines.
+        if line.is_empty() {
+            continue;
+        }
+
+        // Strip comments.
+        let (_, line) = alt((take_until(" #"), rest))(line)?;
+
+        // Parse dependency.
         let (_, pkg) = package(line)?;
         pkgs.push(pkg);
     }
@@ -29,9 +37,27 @@ pub fn parse(input: &str) -> Result<&str, Vec<Package>> {
     Ok((input, pkgs))
 }
 
+/// Parse one line in the lockfile.
+fn line(input: &str) -> Result<&str, &str> {
+    // Take everything until the next newline.
+    //
+    // This takes line continuation characters into account.
+    let (input, mut line) = recognize(parsers::take_continued_line)(input)?;
+
+    // Remove irrelevant whitespace.
+    line = line.trim();
+
+    // Remove entirely commented out lines.
+    if line.starts_with('#') {
+        line = "";
+    }
+
+    Ok((input, line))
+}
+
 fn package(input: &str) -> Result<&str, Package> {
     // Ignore everything after `;`.
-    let (_, input) = recognize(alt((take_until(";"), not_line_ending)))(input)?;
+    let (_, input) = alt((take_until(";"), rest))(input)?;
 
     // Parse for `-e` dependencies.
     if let Ok(editable) = editable(input) {
@@ -151,8 +177,29 @@ fn identifier_list(input: &str) -> Result<&str, &str> {
 }
 
 fn line_done(input: &str) -> Result<&str, &str> {
-    let (input, _) = space0(input)?;
+    // Allow for spaces and arguments not impacting resolution.
+    let (input, _) = recognize(many0(alt((nl_space1, package_hash))))(input)?;
+
     eof(input)
+}
+
+/// Parse package hashes.
+///
+/// Example:
+///   --hash=sha256:
+/// 8c2f9abd47a9e8df7f0c3f091ce9497d011dc3b31effcf4c85a6e2b50f4114ef
+fn package_hash(input: &str) -> Result<&str, &str> {
+    // Argument name.
+    let (input, _) = tag("--hash=")(input)?;
+
+    // Hash variant.
+    let (input, _) = alphanumeric1(input)?;
+
+    // Separator.
+    let (input, _) = tag(":")(input)?;
+
+    // Package hash.
+    alphanumeric1(input)
 }
 
 /// A combinator that takes a parser `inner` and produces a parser that also
@@ -162,5 +209,24 @@ fn ws<'a, F>(inner: F) -> impl FnMut(&'a str) -> Result<&str, &str>
 where
     F: Fn(&'a str) -> Result<&str, &str>,
 {
-    delimited(space0, inner, space0)
+    delimited(nl_space0, inner, nl_space0)
+}
+
+/// Newline-aware space0.
+///
+/// This automatically handles " \\\n" and treats it as normal space.
+fn nl_space0(input: &str) -> Result<&str, &str> {
+    recognize(many0(alt((space1, line_continuation))))(input)
+}
+
+/// Newline-aware space1.
+///
+/// This automatically handles " \\\n" and treats it as normal space.
+fn nl_space1(input: &str) -> Result<&str, &str> {
+    recognize(many1(alt((space1, line_continuation))))(input)
+}
+
+/// Recognize line continuations.
+fn line_continuation(input: &str) -> Result<&str, &str> {
+    recognize(pair(tag("\\"), line_ending))(input)
 }
