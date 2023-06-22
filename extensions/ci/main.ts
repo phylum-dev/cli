381ec2ef4607e4131b6b6e71eaa154a68e483e4f
Christian Duerr <chris.durr@phylum.io>
2023-06-23 00:18:18 +0200

    Add support for multiple lockfiles

--- extensions/ci/main.ts
+++ extensions/ci/main.ts
@@ -1,37 +1,48 @@
 import { PhylumApi } from "phylum";
 
 // Ensure required arguments are present.
-if (Deno.args.length < 4 || Deno.args.length > 5) {
+const args = Deno.args.slice(0);
+if (args.length < 4) {
   console.error(
-    "Usage: phylum ci <PROJECT> [--group <GROUP>] <LABEL> <BASE> <LOCKFILE>",
+    "Usage: phylum ci <PROJECT> [--group <GROUP>] <LABEL> <BASE> <LOCKFILE...>",
   );
   Deno.exit(1);
 }
 
 // Find optional groups argument.
 let group = undefined;
-const groupArgsIndex = Deno.args.indexOf("--group");
+const groupArgsIndex = args.indexOf("--group");
 if (groupArgsIndex != -1) {
-  const groupArgs = Deno.args.splice(groupArgsIndex, groupArgsIndex);
+  const groupArgs = args.splice(groupArgsIndex, groupArgsIndex + 1);
   group = groupArgs[1];
 }
 
 // Parse remaining arguments.
-const project = Deno.args[0];
-const label = Deno.args[1];
-const base = Deno.args[2];
-const lockfile = Deno.args[3];
+const project = args[0];
+const label = args[1];
+const base = args[2];
+const lockfiles = args.splice(3);
 
-// Parse new lockfile.
-const lockfileDeps = await PhylumApi.parseLockfile(lockfile);
+// Parse new lockfiles.
+let packages = [];
+for (const lockfile of lockfiles) {
+  const lockfileDeps = await PhylumApi.parseLockfile(lockfile);
+  packages = packages.concat(lockfileDeps.packages);
+}
 
 // Deserialize base dependencies.
 const baseDepsJson = await Deno.readTextFile(base);
 const baseDeps = JSON.parse(baseDepsJson);
 
+// Short-circuit if there are no dependencies.
+if (packages.length == 0) {
+  console.log("{}");
+  Deno.exit(0);
+}
+
 // Submit analysis job.
 const jobID = await PhylumApi.analyze(
-  lockfileDeps.packages,
+  packages,
   project,
   group,
   label,
