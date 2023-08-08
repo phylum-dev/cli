683b8780aec5b0460ddcd9e734d0f96da8d847e2
Eric Ortega <24722023+ejortega@users.noreply.github.com>
2023-08-08 13:05:50 -0500

    Use tempdir for cross-platform tests
    Co-authored-by: Kyle Willmon <kyle@phylum.io>

--- cli/tests/parse.rs
+++ cli/tests/parse.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use predicates::prelude::*;
+use tempfile::TempDir;
 
 use crate::common::TestCli;
 
@@ -29,18 +30,20 @@ fn parse_with_project_lockfile() {
 #[test]
 fn parse_with_project_lockfile_relative_paths() {
     // Setup CLI with temp dir.
-    let test_cli = TestCli::builder().cwd_temp().build();
-    let temp_path = test_cli.temp_path();
+    let tempdir = TempDir::new().unwrap();
+    let sensitive_dir = tempdir.path().join("sensitive_dir_name");
+    fs::create_dir_all(&sensitive_dir).unwrap();
+    let test_cli = TestCli::builder().cwd(sensitive_dir.clone()).build();
 
     // Write .phylum_project to temp dir.
     let config = "id: 00000000-0000-0000-0000-000000000000\nname: test\ncreated_at: \
                   2000-01-01T00:00:00.0Z\nlockfile_path: ./package-lock.json\nlockfile_type: npm";
-    fs::write(temp_path.join(".phylum_project"), config).unwrap();
+    fs::write(sensitive_dir.join(".phylum_project"), config).unwrap();
 
     // Copy lockfile to temp dir.
-    fs::copy("../tests/fixtures/package-lock.json", temp_path.join("./package-lock.json")).unwrap();
+    fs::copy("../tests/fixtures/package-lock.json", sensitive_dir.join("./package-lock.json"))
+        .unwrap();
 
-    let not_contain_var_folders =
-        predicate::function(|x: &str| !x.contains("\"lockfile\": \"/private/var/folders"));
-    test_cli.cmd().args(["parse"]).assert().success().stdout(not_contain_var_folders);
+    let not_sensitive_dir = predicate::str::contains("sensitive_dir_name").not();
+    test_cli.cmd().args(["parse"]).assert().success().stdout(not_sensitive_dir);
 }
