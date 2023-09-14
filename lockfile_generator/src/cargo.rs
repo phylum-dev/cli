09085882e0ccafbd31bf81866ba558ea078bbc3e
Christian Duerr <chris.durr@phylum.io>
2023-09-14 23:17:59 +0200

    Switch to cargo command

--- lockfile_generator/src/cargo.rs
+++ lockfile_generator/src/cargo.rs
@@ -3,8 +3,7 @@
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-use rust_cargo::core;
-use rust_cargo::util::config::Config;
+use serde::Deserialize;
 
 use crate::{Error, Generator, Result};
 
@@ -12,14 +11,22 @@ pub struct Cargo;
 
 impl Generator for Cargo {
     fn lockfile_path(&self, manifest_path: &Path) -> Result<PathBuf> {
-        let cargo_config = Config::default()?;
-        let workspace_manifest = core::find_workspace_root(manifest_path, &cargo_config)?;
-        let root_manifest =
-            workspace_manifest.as_ref().map(PathBuf::as_path).unwrap_or(manifest_path);
-        let workspace_root = root_manifest
-            .parent()
-            .ok_or_else(|| Error::InvalidManifest(root_manifest.to_path_buf()))?;
-        Ok(workspace_root.join("Cargo.lock"))
+        let manifest_arg = format!("--manifest-path={}", manifest_path.display());
+        let output = Command::new("cargo")
+            .args(["metadata", &manifest_arg, "--format-version=1"])
+            .output()?;
+
+        // Ensure command was successful.
+        if !output.status.success() {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            return Err(Error::NonZeroExit(output.status.code(), stderr.into()));
+        }
+
+        // Parse metadata output.
+        let stdout = String::from_utf8(output.stdout).map_err(Error::InvalidUtf8)?;
+        let metadata: Metadata = serde_json::from_str(&stdout)?;
+
+        Ok(PathBuf::from(metadata.workspace_root).join("Cargo.lock"))
     }
 
     fn command(&self, _manifest_path: &Path) -> Command {
@@ -31,4 +38,10 @@ impl Generator for Cargo {
     fn tool(&self) -> &'static str {
         "Cargo"
     }
+}
+
+/// Output of `cargo metadata`.
+#[derive(Deserialize)]
+struct Metadata {
+    workspace_root: String,
 }
