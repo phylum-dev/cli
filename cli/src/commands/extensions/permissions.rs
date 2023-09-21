phylum-dev/cli

commit e166fceabe239bdeec585f69322d200b7b57c406

Add extension helpers for direct API requests (#868)
* Add extension helpers for direct API requests

This patch adds some new abstractions to the extension API which make it
easier for extension authors to make requests directly to Phylum's REST
API.

To ensure people can figure out how they're supposed to use these new
functions, the functionality has also been documented in our extension
documentation.

Closes #863.
Closes #862.

* Add default Deno exceptons for API subdomains

cli/src/commands/extensions/permissions.rs
@@ -244,16 +244,20 @@ impl From<&Permissions> for PermissionsOptions {
         let allow_write =
             value.write.get().map(|write| write.iter().map(PathBuf::from).collect::<Vec<_>>());
 
-        let allow_env = value.env.get().cloned();
-        let allow_net = value.net.get().cloned();
+        let mut allow_net = value.net.get().cloned().unwrap_or_default();
         let allow_run = value.unsandboxed_run.get().cloned();
+        let allow_env = value.env.get().cloned();
+
+        // Add net exceptions to phylum.io's domains by default.
+        allow_net.push("api.staging.phylum.io".into());
+        allow_net.push("api.phylum.io".into());
 
         PermissionsOptions {
             allow_read,
             allow_write,
-            allow_net,
             allow_run,
             allow_env,
+            allow_net: Some(allow_net),
             allow_sys: None,
             allow_ffi: None,
             allow_hrtime: false,
@@ -357,7 +361,12 @@ mod tests {
         assert!(permissions_options.allow_write.is_none());
         assert!(permissions_options.allow_env.is_none());
         assert!(permissions_options.allow_run.is_none());
-        assert!(permissions_options.allow_net.is_none());
+
+        // NOTE: Net is an exception since we allow our own API domains by default.
+        assert_eq!(
+            permissions_options.allow_net,
+            Some(vec!["api.staging.phylum.io".into(), "api.phylum.io".into()])
+        );
     }
 
     #[test]

cli/src/commands/extensions/api.rs
@@ -473,6 +473,14 @@ fn op_permissions(op_state: Rc<RefCell<OpState>>) -> permissions::Permissions {
     state.extension().permissions().into_owned()
 }
 
+#[op]
+async fn api_base_url(op_state: Rc<RefCell<OpState>>) -> Result<String> {
+    let state = ExtensionState::from(op_state);
+    let api = state.api().await?;
+    let url = api.config().connection.uri.clone() + "/api";
+    Ok(url)
+}
+
 pub(crate) fn api_decls() -> Vec<OpDecl> {
     vec![
         analyze::decl(),
@@ -489,5 +497,6 @@ pub(crate) fn api_decls() -> Vec<OpDecl> {
         parse_lockfile::decl(),
         run_sandboxed::decl(),
         op_permissions::decl(),
+        api_base_url::decl(),
     ]
 }

cli/src/extension_api.ts
@@ -4,6 +4,54 @@
 
 export class PhylumApi {
   /**
+   * Send a request to the Phylum REST API.
+   *
+   * See https://api.staging.phylum.io/api/v0/swagger/index.html for available API endpoints.
+   *
+   * The `init` parameter matches the `init` parameter of the Deno `fetch` function:
+   * https://deno.land/api@latest?s=fetch
+   */
+  static async fetch(
+    apiVersion: ApiVersion | string,
+    endpoint: string,
+    init?: RequestInit,
+  ): Promise<object> {
+    // Ensure header object is initialized.
+    const fetchInit = init ?? {};
+
+    // Ensure consistent headers type.
+    fetchInit.headers = new Headers(fetchInit.headers);
+
+    // Set Authorization header if it is missing.
+    if (!fetchInit.headers.has("Authorization")) {
+      const token = await PhylumApi.getAccessToken();
+      fetchInit.headers.set("Authorization", `Bearer ${token}`);
+    }
+
+    // Set Content-Type header if it is missing.
+    if (fetchInit.headers.has("Content-Type")) {
+      fetchInit.headers.set("Content-Type", "application/json");
+    }
+
+    // Set Content-Type header if it is missing.
+
+    // Get API base URI without version.
+    const baseUrl = await PhylumApi.apiBaseUrl();
+
+    // Send fetch request.
+    return fetch(`${baseUrl}/${apiVersion}${endpoint}`, fetchInit);
+  }
+
+  /**
+   * Get the Phylum REST API base URL.
+   *
+   * This will usually return `https://api.phylum.io/api`.
+   */
+  static async apiBaseUrl(): Promise<URL> {
+    return new URL(await Deno.core.opAsync("api_base_url"));
+  }
+
+  /**
    * Analyze dependencies in a lockfile.
    *
    * Packages are expected in the following format:
@@ -391,4 +439,9 @@ export class PhylumApi {
   static permissions(): object {
     return Deno.core.ops.op_permissions();
   }
+}
+
+/** Available Phylum REST API versions. **/
+export enum ApiVersion {
+  V0 = "v0",
 }

cli/tests/common.rs
@@ -158,8 +158,11 @@ impl<'a> TestExtension<'a> {
 
         // Overwrite skeleton code.
         let main = extension_path.join("main.ts");
-        fs::write(main, format!("import {{ PhylumApi }} from 'phylum';\n{code}").as_bytes())
-            .unwrap();
+        fs::write(
+            main,
+            format!("import {{ PhylumApi, ApiVersion }} from 'phylum';\n{code}").as_bytes(),
+        )
+        .unwrap();
 
         // Overwrite permissions.
         let manifest_path = extension_path.join("PhylumExt.toml");

cli/tests/end_to_end/extension.rs
@@ -185,3 +185,21 @@ pub fn async_state() {
         .run()
         .success();
 }
+
+#[tokio::test]
+pub async fn rest_api() {
+    let test_cli = TestCli::builder().with_config(None).build();
+
+    test_cli
+        .extension(
+            "
+            const reply = await PhylumApi.fetch(ApiVersion.V0, '/health');
+            console.log(await reply.json());
+        ",
+        )
+        .with_permissions(Permissions { net: Permission::Boolean(true), ..Permissions::default() })
+        .build()
+        .run()
+        .success()
+        .stdout("{ response: \"alive!\" }\n");
+}

commit 55931e8c25bb7edff80a7f2a79b4f2938084c436

Fix allow all networking permissions
This patch fixes a regression from e166fce that was causing `net = true`
permissions to be handled like `net = false`. This meant than an
extension using `net = true` would only be able to access the Phylum API
domains from their extension code. Subprocesses and the network sandbox
were unaffected by this regression.

cli/src/commands/extensions/permissions.rs
@@ -44,6 +44,17 @@ impl Permission {
         }
     }
 
+    /// Add resource to allow list
+    pub fn allow_resource(&mut self, resource: String) {
+        match self {
+            Self::Boolean(true) => {},
+            Self::Boolean(false) => {
+                *self = Self::List(vec![resource]);
+            },
+            Self::List(list) => list.push(resource),
+        }
+    }
+
     /// Get Birdcage sandbox exception resource paths.
     #[cfg(unix)]
     pub fn sandbox_paths(&self) -> Cow<'_, Vec<String>> {
@@ -244,20 +255,23 @@ impl From<&Permissions> for PermissionsOptions {
         let allow_write =
             value.write.get().map(|write| write.iter().map(PathBuf::from).collect::<Vec<_>>());
 
-        let mut allow_net = value.net.get().cloned().unwrap_or_default();
         let allow_run = value.unsandboxed_run.get().cloned();
         let allow_env = value.env.get().cloned();
 
+        let mut allow_net = value.net.clone();
+
         // Add net exceptions to phylum.io's domains by default.
-        allow_net.push("api.staging.phylum.io".into());
-        allow_net.push("api.phylum.io".into());
+        allow_net.allow_resource("api.staging.phylum.io".into());
+        allow_net.allow_resource("api.phylum.io".into());
+
+        let allow_net = allow_net.get().cloned();
 
         PermissionsOptions {
             allow_read,
             allow_write,
             allow_run,
             allow_env,
-            allow_net: Some(allow_net),
+            allow_net,
             allow_sys: None,
             allow_ffi: None,
             allow_hrtime: false,
@@ -390,6 +404,26 @@ mod tests {
             permissions_options.allow_net,
             Some(vec!["api.staging.phylum.io".into(), "api.phylum.io".into()])
         );
+    }
+
+    #[test]
+    fn allow_all_becomes_empty_vec() {
+        let permissions = Permissions {
+            read: Permission::Boolean(true),
+            write: Permission::Boolean(true),
+            env: Permission::Boolean(true),
+            run: Permission::Boolean(true),
+            net: Permission::Boolean(true),
+            unsandboxed_run: Permission::Boolean(true),
+        };
+
+        let permissions_options = PermissionsOptions::try_from(&permissions).unwrap();
+
+        assert!(permissions_options.allow_read.unwrap().is_empty());
+        assert!(permissions_options.allow_write.unwrap().is_empty());
+        assert!(permissions_options.allow_env.unwrap().is_empty());
+        assert!(permissions_options.allow_run.unwrap().is_empty());
+        assert!(permissions_options.allow_net.unwrap().is_empty());
     }
 
     #[test]